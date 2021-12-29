2c86ca2740bb0a30bb860d332d15b82e8def9228
yooji0415 <gch05406@gmail.com>
2021-12-30 00:52:52 +0900

    programmers Lv1 Keypad

--- Lv1_Lotto.py
+++ Lv1_Lotto.py
@@ -26,27 +26,24 @@ def solution(lottos, win_nums):
     return answer
 
 
-"""
-모범 답안
-    def solution(lottos, win_nums):
+# 모범 답안
+def best_solution(lottos, win_nums):
         
-        등수와 맞은 개수의 합과 연관이 있다는 것은 확인했으나
-        이렇게 리스트 구조로 작성시 인덱스와 연결할 수 있다는 
-        생각을 못했다.
-        rank = [6, 6, 5, 4, 3, 2, 1] 
+    # 등수와 맞은 개수의 합과 연관이 있다는 것은 확인했으나
+    # 이렇게 리스트 구조로 작성시 인덱스와 연결할 수 있다는
+    # 생각을 못했다.
+    rank = [6, 6, 5, 4, 3, 2, 1]
         
-        파이썬의 내장함수 count를 이용하면 더 쉽게 할 수 있다.
-        cnt_0 = lottos.count(0)
-        ans = 0
+    # 파이썬의 내장함수 count 를 이용하면 더 쉽게 할 수 있다.
+    cnt_0 = lottos.count(0)
+    ans = 0
         
-        맞은 개수 또한 파이썬의 리스트 for문 연산 방법을 
-        사용하면 더 쉽게 해결할 수 있다.
-        for x in win_nums:
-            if x in lottos:
-                ans += 1
+    # 맞은 개수 또한 파이썬의 리스트 for 문 연산 방법을
+    # 사용하면 더 쉽게 해결할 수 있다.
+    for x in win_nums:
+        if x in lottos:
+            ans += 1
                 
-        리스트 변수를 이용한 return이 아닌 개별 return도 
-        리스트 구조로 전달이 되는 것을 알게 되었다.
-        return rank[cnt_0 + ans], rank[ans]
-"""
-
+    # 리스트 변수를 이용한 return 이 아닌 개별 return 도
+    # 리스트 구조로 전달이 되는 것을 알게 되었다.
+    return rank[cnt_0 + ans], rank[ans]
--- Lv1_Number_String.py
+++ Lv1_Number_String.py
@@ -23,16 +23,18 @@ def solution(s):
                 eng = ''
 
     return int(answer)
-"""
-모범답안
-    num_dic = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", 
-                "six":"6", "seven":"7", "eight":"8", "nine":"9"}
 
-    def solution(s):
-        answer = s
-        # key 값을 찾았을 때 바로 value로 바꿔버린다. 
-        # replace 함수에 대한 이해와 센스가 중요했다.
-        for key, value in num_dic.items():
-            answer = answer.replace(key, value)
-        return int(answer)
-"""
+
+# 모범답안
+num_dic = {"zero": "0", "one": "1", "two": "2", "three": "3", "four": "4", "five": "5",
+           "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+
+
+def solution(s):
+    answer = s
+    # key 값을 찾았을 때 바로 value 로 바꿔버린다.
+    # replace 함수에 대한 이해와 센스가 중요했다.
+    for key, value in num_dic.items():
+        answer = answer.replace(key, value)
+
+    return int(answer)
