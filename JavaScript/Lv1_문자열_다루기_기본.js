3e980241835f8bb34801cc8e2e0613762a0d1fcb
yooji0415 <gch05406@gmail.com>
2022-08-14 22:23:42 +0900

    JavaScript / 1 problem

--- JavaScript/Lv1_문자열_다루기_기본.js
+++ JavaScript/Lv1_문자열_다루기_기본.js
@@ -1,6 +1,6 @@
 function solution(s) {
   if (s.length === 4 || s.length === 6) {
-    return s.split("").every((c) => !isNaN(c)); // 모든 요소가 number이면 true리턴
+    return s.split("").every((c) => !isNaN(c));
   } else {
     return false;
   }
