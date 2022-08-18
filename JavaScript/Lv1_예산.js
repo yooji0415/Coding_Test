0aac7a52d53a15a7c6bf18e3ebd4fedbe2c69c33
yooji0415 <gch05406@gmail.com>
2022-08-18 21:00:40 +0900

    JavaScript / 1 problem

--- JavaScript/Lv1_예산.js
+++ JavaScript/Lv1_예산.js
@@ -2,4 +2,12 @@ function solution(d, budget) {
   const sortedD = d.sort((a, b) => {
     return a - b;
   });
+  let answer = 0;
+  sortedD.forEach((value) => {
+    if(budget - value >= 0){
+        answer++;
+        budget -= value;
+    }
+  });
+  return answer;
 }
